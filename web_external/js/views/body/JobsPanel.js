22e739e123a321ae50ef1d9bc538d2cc5828aae1
Michael Grauer <michael.grauer@kitware.com>
2015-08-12 23:39:53 +0000

    WIP: job list integration

--- web_external/js/views/body/JobsPanel.js
+++ web_external/js/views/body/JobsPanel.js
@@ -1,10 +1,19 @@
 minerva.views.JobsPanel = minerva.View.extend({
 
     initialize: function () {
+        this.jobListWidget = new girder.views.jobs_JobListWidget({
+            showHeader: false,
+            pageLimit: 10,
+            parentView: this
+        });
+        var columns = this.jobListWidget.columnEnum.COLUMN_STATUS_ICON |
+                      this.jobListWidget.columnEnum.COLUMN_TITLE;
+        this.jobListWidget.columns = columns;
     },
 
     render: function () {
         this.$el.html(minerva.templates.jobsPanel({}));
+        this.jobListWidget.setElement(this.$('.m-jobsListContainer'));
 
         return this;
     }
